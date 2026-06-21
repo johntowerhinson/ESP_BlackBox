9aa6f6f05b680c81d48291527548411b9ba2c0e8
John Tower <johntowerhinson@gmail.com>
2026-06-21 06:15:32 -0700

    IMU and GPS data successfully syncs to webui. Magnometer needs initialization (its on aux I2C bus of GY-91)

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ void setup() {
 
     bmp.begin(0x76);
     mpu.init(); // Correct initialization for MPU9250_WE
+    mpu.initMagnetometer();
     mpu.autoOffsets(); // Highly recommended for MPU9250_WE
 
     WiFi.softAP("ESP_BlackBox", "password123");
@@ -35,24 +36,29 @@ void setup() {
     });
     
     server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request) {
-        JsonDocument doc; // New standard for v7
-        
-        // BMP Data
-        doc["temp"] = bmp.readTemperature();
-        doc["press"] = bmp.readPressure() / 100.0F;
-        
-        // MPU9250_WE Data
-        xyzFloat acc = mpu.getGValues();
-        doc["accelX"] = acc.x;
-        
-        // GPS Data
-        doc["lat"] = gps.location.lat();
-        doc["lng"] = gps.location.lng();
-        
-        String output;
-        serializeJson(doc, output);
-        request->send(200, "application/json", output);
-    });
+    JsonDocument doc;
+    
+    // BMP280
+    doc["temp"] = bmp.readTemperature();
+    doc["press"] = bmp.readPressure() / 100.0F;
+    
+    // MPU9250_WE (9-Axis)
+    xyzFloat acc = mpu.getGValues();
+    xyzFloat gyr = mpu.getGyrValues();
+    xyzFloat mag = mpu.getMagValues();
+    doc["accX"] = acc.x; doc["accY"] = acc.y; doc["accZ"] = acc.z;
+    doc["gyrX"] = gyr.x; doc["gyrY"] = gyr.y; doc["gyrZ"] = gyr.z;
+    doc["magX"] = mag.x; doc["magY"] = mag.y; doc["magZ"] = mag.z;
+    
+    // GPS
+    doc["lat"] = gps.location.lat();
+    doc["lng"] = gps.location.lng();
+    doc["time"] = String(gps.time.hour()) + ":" + String(gps.time.minute());
+    
+    String output;
+    serializeJson(doc, output);
+    request->send(200, "application/json", output);
+});
 
     server.begin();
 }
