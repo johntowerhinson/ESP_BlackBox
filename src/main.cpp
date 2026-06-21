e552ca0bbd209d8957d2a9b8946e58e5fcac04ba
John Tower <johntowerhinson@gmail.com>
2026-06-21 05:36:29 -0700

    working webview reporting live GPS data

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,62 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
 #include <Adafruit_BMP280.h>
+#include <MPU9250_WE.h> // Ensure you use this header
+#include <TinyGPS++.h>
+#include <ArduinoJson.h>
 #include <Wire.h>
-
-const char* ssid = "4RunnerOS-HUD";
-const char* password = "password123";
 
 AsyncWebServer server(80);
 Adafruit_BMP280 bmp;
+MPU9250_WE mpu = MPU9250_WE(0x68); // Usually 0x68 or 0x69
+TinyGPSPlus gps;
+HardwareSerial gpsSerial(1);
 
 void setup() {
     Serial.begin(115200);
-
-    // Initialize Filesystem
-    if (!LittleFS.begin(true, "/spiffs")) {
+    gpsSerial.begin(9600, SERIAL_8N1, 16, 17);
+    if (!LittleFS.begin(true)) { 
     Serial.println("LittleFS Mount Failed!");
 } else {
     Serial.println("LittleFS Mounted!");
 }
-
-    // Initialize I2C and Sensor
     Wire.begin(21, 22);
-    if (!bmp.begin(0x76)) Serial.println("BMP280 not found");
 
-    // Initialize WiFi
-    WiFi.softAP(ssid, password);
-    
-    // Serve HTML from LittleFS
+    bmp.begin(0x76);
+    mpu.init(); // Correct initialization for MPU9250_WE
+    mpu.autoOffsets(); // Highly recommended for MPU9250_WE
+
+    WiFi.softAP("ESP_BlackBox", "password123");
+
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(LittleFS, "/index.html", "text/html");
     });
+    
+    server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request) {
+        JsonDocument doc; // New standard for v7
+        
+        // BMP Data
+        doc["temp"] = bmp.readTemperature();
+        doc["press"] = bmp.readPressure() / 100.0F;
+        
+        // MPU9250_WE Data
+        xyzFloat acc = mpu.getGValues();
+        doc["accelX"] = acc.x;
+        
+        // GPS Data
+        doc["lat"] = gps.location.lat();
+        doc["lng"] = gps.location.lng();
+        
+        String output;
+        serializeJson(doc, output);
+        request->send(200, "application/json", output);
+    });
 
     server.begin();
-    Serial.println("Web server started at http://192.168.4.1");
 }
 
 void loop() {
-    // Keep this clean for now
+    while (gpsSerial.available() > 0) gps.encode(gpsSerial.read());
 }
